561db15b084c436b64e78caf3792f09ad22e0535
coderguang <royalchen@royalchen.com>
2015-06-23 09:39:15 +0800

    simple chat server complete

--- server/server.cpp
+++ server/server.cpp
@@ -1,6 +1,7 @@
-#include "../ComNet.h"
+#include "../../ComLib/ComNet.h"
 #include "Config.h"
 #include <iostream>
+#include <stdlib.h>
 #include <string.h>
 #include <arpa/inet.h>
 #include <sys/types.h>
@@ -14,12 +15,13 @@ int main(int argc,char **argv){
 	struct sockaddr_in cliaddr;
 	struct sockaddr_in servaddr;
 	
-	Socket(AF_INET,SOCK_STREAM,0);
+	listenfd=Socket(AF_INET,SOCK_STREAM,0);
 	
 	bzero(&servaddr,sizeof(servaddr));
 
 	servaddr.sin_family=AF_INET;
 	servaddr.sin_port=htons(PORT);
+	//servaddr.sin_port=htons(atoi(argv[0]));
 	servaddr.sin_addr.s_addr=INADDR_ANY;
 
 	Bind(listenfd,(struct sockaddr*)&servaddr,sizeof(servaddr));
@@ -29,7 +31,7 @@ int main(int argc,char **argv){
 	while(true){
 		socklen_t len=sizeof(cliaddr);
 
-		connfd=Accept(listenfd,(struct sockaddr *)&cliaddr,&len);
+		connfd=accept(listenfd,(struct sockaddr *)&cliaddr,&len);
 		
 		if(connfd==ERROR){
 			continue;
